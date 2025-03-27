1e43e48aaf0a0d1efbb9059f1a9b55be333e5469
makwanasumit <sumitmakwanaa@gmail.com>
2025-03-27 14:28:00 +0530

    prisma stage 1

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,12 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { Prisma } from "@prisma/client"
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-
+    adapter: PrismaAdapter(Prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
